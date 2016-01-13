af21ae3b8702fe0f097fb63d52e74927e767faae
Tristram Oaten <tristram@oaten.name>
2016-01-13 16:45:15 +0000

    redis

--- app.py
+++ app.py
@@ -1,50 +1,40 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
 from flask.ext import admin
 from flask import render_template
-from flask.ext.admin.contrib.mongoengine import ModelView
-from flask.ext.mongoengine import MongoEngine
-from mongoengine import connect
-
-from models import *
-from admin import UserView
+from redis import Redis, StrictRedis
+from flask_admin.contrib import rediscli
+from flask_restful import Resource, Api
 
 # Create application
 app = Flask(__name__)
 app.debug=True
 # Create dummy secrey key so we can use sessions
 app.config['SECRET_KEY'] = '123456790'
-app.config['MONGODB_SETTINGS'] = {
-    'DB': 'testing'
-}
-
-# Create models
-connect(
-    'flask-basic',
-    host=os.environ.get(
-        'MONGOLAB_URI',
-        'mongodb://db/restr'
-    )
-)
-db = MongoEngine()
-db.init_app(app)
-
+r = StrictRedis(host='db', port=6379, db=0)
 
 # Flask views
 @app.route('/')
 def index():
     return render_template('index.html')
 
+api = Api(app)
+
+class Store(Resource):
+    def get(self, key):
+        return r.get(key)
+
+    def put(self, key):
+        return r.set(key, request.form['data'])
+
+api.add_resource(Store, '/store/<string:key>')
+
 
 if __name__ == '__main__':
     # Create admin
     admin = admin.Admin(app, 'Simple Models')
-
-    # Add views
-    admin.add_view(UserView(User))
-    admin.add_view(ModelView(Record))
-
+    admin.add_view(rediscli.RedisCli(Redis(host='db')))
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
